c67bbc46b6e462376b648237830cbe8d1d9b46fe
Buck Doyle <b@chromatin.ca>
2016-06-26 11:27:29 -0400

    Add Math package for importing

---
+++ app/initializers/package-math.js
@@ -0,0 +1,12 @@
+define('math', [], function() {
+  console.log("I am happening!");
+  return Math;
+})
+export function initialize(/* application */) {
+  /* globals define */
+};
+
+export default {
+  name: 'package-math',
+  initialize
+};
--- app/routes/custom-base.js
+++ app/routes/custom-base.js
@@ -1,7 +1,9 @@
 import Ember from 'ember';
+import { default as math, PI } from 'math';
 
 export default Ember.Route.extend({
   afterModel(model) {
     console.log(model || 'no model');
+    console.log(math.sqrt(16), PI);
   }
 });
