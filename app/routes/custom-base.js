2dc7c7ab5a7d2d41f7ca9fd117a0bfaf71b0a923
Buck Doyle <b@chromatin.ca>
2016-06-26 16:32:58 -0400

    Add addon with importable vendor code

--- app/routes/custom-base.js
+++ app/routes/custom-base.js
@@ -1,10 +1,12 @@
 import Ember from 'ember';
 import { default as math, PI } from 'math';
+import GeneralStrike from 'general-strike';
 
 export default Ember.Route.extend({
   afterModel(model) {
     console.log(model || 'no model');
     console.log(math.sqrt(16), PI);
+    console.log(`strike: ${GeneralStrike}`);
   },
 
   model() {
